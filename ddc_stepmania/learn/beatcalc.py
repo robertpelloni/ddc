bafdfc461eb3e5a2bb1acdfd159994da8288c492
mario <mario@mariozechner.at>
2026-04-21 14:54:50 -0400

    chore: workspace sync v2.1.0 - commit working changes

--- ddc_stepmania/learn/beatcalc.py
+++ ddc_stepmania/learn/beatcalc.py
@@ -2,7 +2,6 @@ import numpy as np
 
 
 _EPSILON = 1e-6
-<<<<<<< HEAD:ddc_stepmania/learn/beatcalc.py
 class BeatCalc(object):
     # for simplicity, we will represent a "stop" as an impossibly sharp tempo change
     def __init__(self, offset, beat_bpm, beat_stop):
@@ -27,31 +26,6 @@ class BeatCalc(object):
                 stops[beat] += stop
             stops[beat] = stop
         beat_stop = list(filter(lambda x: x[1] != 0.0, sorted(stops.items(), key=lambda x: x[0])))
-=======
-class BeatTimeCalc(object):
-  # for simplicity, we will represent a "stop" as an impossibly sharp tempo change
-  def __init__(self, offset, beat_bpm, beat_stop):
-    # ensure all beat markers are strictly increasing
-    assert beat_bpm[0][0] == 0.0
-    beat_last = -1.0
-    bpms = []
-    for beat, bpm in beat_bpm:
-      assert beat >= beat_last
-      if beat == beat_last:
-        bpms[-1] = (beat, bpm)
-      else:
-        bpms.append((beat, bpm))
-      beat_last = beat
-
-    # aggregate repeat stops
-    stops = {}
-    for beat, stop in beat_stop:
-      assert beat > 0.0
-      if beat in stops:
-        stops[beat] += stop
-      stops[beat] = stop
-    beat_stop = filter(lambda x: x[1] != 0.0, sorted(stops.items(), key=lambda x: x[0]))
->>>>>>> origin/master_v2:ddc/beattimecalc.py
 
     self.offset = offset
     self.bpms = beat_bpm
@@ -93,7 +67,6 @@ class BeatTimeCalc(object):
     spb = self.segment_spb[seg_idx]
     return time_left + ((beat - beat_left) * spb)
 
-<<<<<<< HEAD:ddc_stepmania/learn/beatcalc.py
 if __name__ == '__main__':
     bc = BeatCalc(0.05, [(0.0, 120.0), (32.0, 60.0), (64.0, 120.0)], [(16.0, 5.0)])
     print(bc.beat_to_time(0.0))
@@ -104,13 +77,3 @@ if __name__ == '__main__':
     print('-' * 80)
     print(bc.time_to_beat(0.0))
     print(bc.time_to_beat(1.0))
-=======
-
-  def time_to_beat(self, time):
-    assert time >= 0.0
-    seg_idx = np.searchsorted(self.segment_time, time, side='right') - 1
-    time_left = self.segment_time[seg_idx]
-    beat_left = self.segment_beat[seg_idx]
-    bps = self.segment_bps[seg_idx]
-    return beat_left + ((time - time_left) * bps)
->>>>>>> origin/master_v2:ddc/beattimecalc.py
